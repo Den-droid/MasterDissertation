8fd4b7b0a248d8393b05303753c7d911c393c027
DESKTOP-LN2JQT3\denis <amerscan8@gmail.com>
2025-11-04 20:32:35 -0500

    function UI is almost done

--- FrontendApplication/frontend-project/src/app/function/function-modal/function-modal.component.ts
+++ FrontendApplication/frontend-project/src/app/function/function-modal/function-modal.component.ts
@@ -1,13 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subject } from 'rxjs';
 import { ActionType } from '../../shared/constants/action-type';
-import { AddSubjectDto, SubjectDto, UpdateSubjectDto } from '../../shared/models/subject.model';
+import { SubjectDto } from '../../shared/models/subject.model';
 import { UniversityDto } from '../../shared/models/university.model';
-import { FunctionDto } from '../../shared/models/function.model';
+import { AddFunctionDto, FunctionDto, UpdateFunctionDto } from '../../shared/models/function.model';
 import { functionLabels } from '../../shared/translations/function.translation';
-import { integerValidator } from '../../shared/validators/number.validator';
+import { decimalValidator, integerValidator } from '../../shared/validators/number.validator';
 import { validationMessages } from '../../shared/translations/common.translation';
 
 @Component({
@@ -32,6 +32,22 @@ export class FunctionModalComponent {
       this.form.get('variablesCount')?.setValue(val.variablesCount);
       this.form.get('subject')?.setValue(val.subject.id);
       this.form.get('university')?.setValue(val.subject.university.id);
+
+      if (val.minValues.length > 0) {
+        (this.form.get('minValues') as FormGroup).get('0')?.setValue(val.minValues[0]);
+        for (let i = 1; i < val.minValues.length; i++) {
+          (this.form.get('minValues') as FormGroup).addControl(i.toString(),
+            new FormControl(val.minValues[i], decimalValidator()));
+        }
+      }
+
+      if (val.maxValues.length > 0) {
+        (this.form.get('maxValues') as FormGroup).get('0')?.setValue(val.maxValues[0]);
+        for (let i = 0; i < val.maxValues.length; i++) {
+          (this.form.get('maxValues') as FormGroup).addControl(i.toString(),
+            new FormControl(val.maxValues[i], decimalValidator()));
+        }
+      }
     }
   }
   get inputValue() {
@@ -85,18 +101,21 @@ export class FunctionModalComponent {
     this.form = this.fb.group({
       text: ['', [Validators.required]],
       variablesCount: ['', [Validators.required, integerValidator()]],
-      minValue: [''],
-      maxValue: [''],
+      minValues: this.fb.group({}),
+      maxValues: this.fb.group({}),
       university: [''],
       subject: ['', [Validators.required]]
     });
+
+    (this.form.get('minValues') as FormGroup).addControl('0', new FormControl('', decimalValidator()));
+    (this.form.get('maxValues') as FormGroup).addControl('0', new FormControl('', decimalValidator()));
 
     this.form.get('university')?.valueChanges.subscribe({
       next: (value: any) => {
         if (value && Number.parseInt(value)) {
           this.displayedSubjects = this.subjects.filter(s => s.university.id == value);
           if (this.selectedSubject &&
-              this.displayedSubjects.filter(ds => ds.id == this.selectedSubject.id).length === 0) {
+            this.displayedSubjects.filter(ds => ds.id == this.selectedSubject.id).length === 0) {
             this.form.get('subject')?.setValue('');
           }
         } else {
@@ -123,13 +142,30 @@ export class FunctionModalComponent {
       return;
     }
 
+    const minValuesToSend = Object.values(this.form.value.minValues)
+      .filter(mv => mv !== '')
+      .map(mv => Number.parseInt(mv as string));
 
+    const maxValuesToSend = Object.values(this.form.value.maxValues)
+      .filter(mv => mv !== '')
+      .map(mv => Number.parseInt(mv as string));
 
-    // if (this.actionType === ActionType.CREATE) {
-    //   this.saveAttempt.emit();
-    // } else {
-    //   this.saveAttempt.emit();
-    // }
+    if (minValuesToSend.length === 0 && maxValuesToSend.length === 0) {
+      this.errorMessage = this.minMaxValueRequiredMessage;
+      return;
+    }
+
+    if (this.actionType === ActionType.CREATE) {
+      let dto = new AddFunctionDto(this.form.value.text, Number.parseInt(this.form.value.variablesCount),
+        minValuesToSend, maxValuesToSend, Number.parseInt(this.form.value.subject));
+
+      this.saveAttempt.emit(dto);
+    } else {
+      let dto = new UpdateFunctionDto(this.form.value.text, Number.parseInt(this.form.value.variablesCount),
+        minValuesToSend, maxValuesToSend, Number.parseInt(this.form.value.subject));
+
+      this.saveAttempt.emit(dto);
+    }
   }
 
   validate() {
@@ -146,4 +182,73 @@ export class FunctionModalComponent {
   isSubjectSelected(id: number) {
     return this.selectedSubject && this.selectedSubject.id === id;
   }
+
+  removeMaxValue(index: number) {
+    const group = (this.form.get('maxValues') as FormGroup);
+    group.removeControl(Object.keys(group.controls)[index]);
+  }
+
+  addMaxValue() {
+    const group = (this.form.get('maxValues') as FormGroup);
+    const controlNames = Object.keys(group.controls);
+    group.addControl(((Number.parseInt(controlNames[controlNames.length - 1]) + 1)).toString(), new FormControl('',
+      decimalValidator()
+    ));
+  }
+
+  removeMinValue(index: number) {
+    const group = (this.form.get('minValues') as FormGroup);
+    group.removeControl(Object.keys(group.controls)[index]);
+  }
+
+  addMinValue() {
+    const group = (this.form.get('minValues') as FormGroup);
+    const controlNames = Object.keys(group.controls);
+    group.addControl(((Number.parseInt(controlNames[controlNames.length - 1]) + 1)).toString(), new FormControl('',
+      decimalValidator()
+    ));
+  }
+
+  isSingleMaxValue(index: number) {
+    const group = (this.form.get('maxValues') as FormGroup);
+    const controlNames = Object.keys(group.controls);
+    return controlNames.length === 1 && index === 0;
+  }
+
+  isLastMaxValue(index: number) {
+    const group = (this.form.get('maxValues') as FormGroup);
+    const controlNames = Object.keys(group.controls);
+    return controlNames.length - 1 === index;
+  }
+
+  isSingleMinValue(index: number) {
+    const group = (this.form.get('minValues') as FormGroup);
+    const controlNames = Object.keys(group.controls);
+    return controlNames.length === 1 && index === 0;
+  }
+
+  isLastMinValue(index: number) {
+    const group = (this.form.get('minValues') as FormGroup);
+    const controlNames = Object.keys(group.controls);
+    return controlNames.length - 1 === index;
+  }
+
+  getMinFormControlName(index: number) {
+    const group = (this.form.get('minValues') as FormGroup);
+    return Object.keys(group.controls)[index];
+  }
+
+  getMaxFormControlName(index: number) {
+    const group = (this.form.get('maxValues') as FormGroup);
+    return Object.keys(group.controls)[index];
+  }
+
+  getMinValues() {
+    return Object.values(this.form.value.minValues);
+  }
+
+  getMaxValues() {
+    return Object.values(this.form.value.maxValues);
+  }
 }
+
